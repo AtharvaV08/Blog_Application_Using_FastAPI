dd9d09edb509f96bcdeaaed75cf346ce7f5f9c4a
Atharva_Vaidya <atharvavaidya08@gmail.com>
2026-04-15 00:26:20 +0530

    post endpoint & snippets added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {"message": "Hello Wrodl!"}
+    return {"message": "Hello World!"}
+
+@app.get("/api/posts")
+def get_posts():
+    with open("snippets.txt", "r") as file:
+        data = file.read()
+    return data
+
+# print(get_posts())
